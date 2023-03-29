bf898f747f2e6ec4c3b02a56194fa6cf7ce08c22
suvratsharma1 <suvrat.spn64@gmail.com>
2023-03-29 17:00:34 +0530

    backticks and print statement learned

---
+++ Learn_js/index1.js
@@ -0,0 +1,15 @@
+var str;
+//backslash n "\n" for changing the line 
+str ="hello how are you \n i am fine"  //to print in next line
+console.log(str)
+//or we can also do this
+var str1;
+//we can print multi line using backticks
+str1 =`thats another way to print 
+ without using backslash n`
+console.log(str1)
+
+// learning print statement
+var a=200;
+// it will compute whats inside the box first
+console.log(`half of ${a} is ${a/2}`) // this is container like %d in c language
