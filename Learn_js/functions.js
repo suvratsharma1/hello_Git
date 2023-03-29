f535907d67a83fdc66fd4aa8dc15edf0530beb4c
suvratsharma1 <suvrat.spn64@gmail.com>
2023-03-30 01:10:32 +0530

    normal_functions_learned

---
+++ Learn_js/functions.js
@@ -0,0 +1,23 @@
+//there are 3 types of functions in javscript
+
+//Normal function
+//function definition
+
+// function function_name(parameter1,parameter2){
+//     statement1;
+//     statement2;
+// }
+
+//Invoking a function
+//function_name(parameter1,parameter2)
+
+function add(a,b){
+    return a+b
+}
+let ans = add(5,6);
+console.log(ans)
+
+//functions are treated as first class citizen in javscript
+//functions can be returned
+//functions can  be passed as parameters/argument
+
