4b462c945bda4fc85778373888100242c2a49e47
suvratsharma1 <suvrat.spn64@gmail.com>
2023-03-29 16:34:08 +0530

    js data types learned

---
+++ Learn_js/index.js
@@ -0,0 +1,36 @@
+//declaring a variable
+var a;  //int float string
+
+//Number
+a=10;                           //assigning value to the variable
+console.log(a);                 //printing the output
+console.log(typeof(a))          //printing the data type of the variable
+var float = 2.4
+console.log(float)
+console.log(typeof float)
+
+//Strings
+
+// string can be enclosed by "", '', and ``
+// ie diuble quotes single quotes and backticks
+a= "helloworld";
+console.log(a);
+console.log(typeof(a))
+c='namste' // also a string
+console.log(c)
+console.log(typeof c)
+
+//Boolean
+a= true;
+console.log(a)
+console.log(typeof(a))
+
+//null object
+a=null
+console.log(a)
+console.log(typeof(a))
+console.log(a>0)
+console.log(a<0)
+console.log(a==0)
+console.log(a>=0)
+console.log(a<=0)
