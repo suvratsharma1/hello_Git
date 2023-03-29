f8f1fc84af02e00711e193df44aca764d3987cd7
suvratsharma1 <suvrat.spn64@gmail.com>
2023-03-29 20:59:36 +0530

    loops learned

---
+++ Learn_js/loops.js
@@ -0,0 +1,11 @@
+//loops in javascript
+// for(intialization;condition;updation){
+//     statement1
+//     statement2
+//     statement3 and soo on
+// }
+for(var a=0;a<10;a++){
+    if(a%2==0){
+        console.log(a);
+    }
+}
