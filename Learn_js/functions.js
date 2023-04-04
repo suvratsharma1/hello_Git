c021d140d495083c25d0595bdd390583b89cf0f7
suvratsharma1 <suvrat.spn64@gmail.com>
2023-04-04 18:57:05 +0530

    functions learned

--- Learn_js/functions.js
+++ Learn_js/functions.js
@@ -21,3 +21,28 @@ console.log(ans)
 //functions can be returned
 //functions can  be passed as parameters/argument
 
+function calculator(str,a,b){
+    if(str="add"){
+        return function add(){   //we can return the whole function in javascript
+            console.log(a+b);
+        }
+    }
+}
+
+let returnedfunc = calculator("add",4,5); //here the returnedfunc becomes the function"it doesn't store the values from calculator function instead it stores the whole add function in it"
+console.log("returned func is "+ returnedfunc);
+returnedfunc(); //this is called function expression  //so when we call this variable we are just calling the add function
+
+//Let's just take another example
+let arr = function abcd(){         //arr will store the whole function definition of abcd
+    console.log("this is a function expression")
+}
+arr(); //so here this will work
+//abcd(); // but this will give "abcd is not defined" error
+
+//lets talk about IIFE-> Immediately invoke function experession
+
+let addIIFE = (function addd(a,b){
+    console.log(a+b);
+})(20,30);
+//console.log(addIIFE);
