9a461cfa02365807b564d29956e73f3a14a5704f
suvratsharma1 <suvrat.spn64@gmail.com>
2023-04-05 18:23:52 +0530

    arrays learned

---
+++ Learn_js/arrays.js
@@ -0,0 +1,6 @@
+// Arrays-> Arrays are the collection of elements 
+let arr =["car","bike","truck",1,2,3,true];
+// in js we can store values of multiple data types 
+console.log(arr);  //this will print the whole array
+//in  js we don't need to iterate through the whole array to print it
+
---
+++ Learn_js/index3.js
@@ -0,0 +1,5 @@
+console.log(x); // this will result in giving o/p "undefined", concept of exeecution context and call stack
+var x=20;
+
+let add = function add(){console.log("how you doin?")}
+console.log("srprmf"+add)
